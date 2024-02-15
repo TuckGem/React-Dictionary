9c1253e58ec163e2ad15333dae942399d78bc7a4
TuckGem <141772389+TuckGem@users.noreply.github.com>
2024-02-15 09:18:07 +0000

    Create Results.js

---
+++ src/Results.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Meaning from "./Meaning";
+import "./Results.css";
+
+export default function Results(props) {
+  if (props.results) {
+    return (
+      <div className="Results">
+        <section>
+          <h1>{props.results.word}</h1>
+          <h2>{props.results.phonetic}</h2>
+        </section>
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
