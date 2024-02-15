eed5ff51257a851fadd96260c34be4cb8872b461
TuckGem <141772389+TuckGem@users.noreply.github.com>
2024-02-15 09:43:12 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,39 @@
+import "./App.css";
+import Dictionary from "./Dictionary";
 
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <h1 className="dictionary-title">Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary defaultKeyword="book" />
+        </main>
+        <footer>
+          <a
+            className="gitLink"
+            href="
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source
+          </a>{" "}
+          code by{" "}
+          <a
+            className="myWebsite"
+            href=""
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
